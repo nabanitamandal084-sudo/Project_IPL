37cd4ad974a0bd36a8f01e4352a942b5c52a4104
nabanitamandal084-sudo <nabanitamandal084@gmail.com>
2026-06-14 01:26:37 +0530

    Update IPL  Batsmen Practice.sql

--- IPL  Batsmen Practice.sql
+++ IPL  Batsmen Practice.sql
@@ -1,16 +1,1 @@
-1.Which batsmen scored the most runs in the 2018 season while maintaining an average strike rate of at least 130?
-select player,Team,Runs,S_R
-from `nabanita9836.ipl_analysis9836.2018_Batsmen_new`
-where S_R>= 130
-order by Runs DESC;
 
-2.Which batsmen scored the most runs across both 2018 and 2019 seasons?
-SELECT Player,SUM(Runs) AS Total_Runs
-FROM (
-SELECT Player, Runs
-FROM `nabanita9836.ipl_analysis9836.2018_Batsmen_new`
-UNION ALL
-SELECT Player, Runs
-FROM `nabanita9836.ipl_analysis9836.2018_Batsmen_new`)
-GROUP BY Player
-ORDER BY Total_Runs DESC;
